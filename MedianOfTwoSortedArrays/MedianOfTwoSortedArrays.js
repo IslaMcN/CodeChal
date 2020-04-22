ebb049fb9167ac0cdd4da0087549f5679a7537d8
Isla <fallenacademy@gmail.com>
2020-04-22 09:03:05 -0700

    Added median of two sorted arrays

---
+++ MedianOfTwoSortedArrays/MedianOfTwoSortedArrays.js
@@ -0,0 +1,12 @@
+var findMedianSortedArrays = function(nums1, nums2) {
+    const l = nums1.length + nums2.length
+    let last;
+    for(let i = 0; i < l/2; i+=1){
+        last = min(nums1, nums2)
+    }
+    return l%2 ? last : (last + min(nums1, nums2))/2
+};
+
+function min(n1, n2){
+    return n1.length && n2.length ? (n2[0] > n1[0] ? n1.shift() : n2.shift()) : (n1.length ? n1.shift() : n2.shift())
+}
